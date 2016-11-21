693ca592d7b33ecbe8819d81e17e81a3f4b0e4fe
Admin <1033948449@qq.com>
2016-11-21 17:25:37 +0800

    Initial Commit

---
+++ FootTec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FootTec
+//
+//  Created by Admin on 16/11/21.
+//  Copyright © 2016年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
